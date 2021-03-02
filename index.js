a25c2106ccf962e6abbd463ca16bb9cc65b52342
syakhisk <mail.syakhisk@gmail.com>
2021-03-02 18:46:42 +0700

    init express

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
 
-(async () => {
+const getBody = async () => {
 	const chromeOptions = {
 		headless: true,
 		defaultViewport: null,
@@ -13,6 +13,18 @@ const puppeteer = require("puppeteer");
 	let bodyHTML = await page.evaluate(() => document.body.innerHTML);
 	// await page.screenshot({ path: "example.png" });
 	console.log(bodyHTML);
-
 	await browser.close();
-})();
+
+	return bodyHTML;
+};
+
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+	res.send(getBody);
+});
+
+app.listen(process.env.PORT || port, () =>
+	console.log(`Listening at port ${port}`)
+);
