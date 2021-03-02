3999697850f0dbe84158d5eb3a1318a5be211ba1
syakhisk <mail.syakhisk@gmail.com>
2021-03-02 18:26:13 +0700

    screenshot test

---
+++ index.js
@@ -0,0 +1,16 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+	const chromeOptions = {
+		headless: true,
+		defaultViewport: null,
+		args: ["--incognito", "--no-sandbox", "--single-process", "--no-zygote"],
+	};
+	const browser = await puppeteer.launch(chromeOptions);
+	const page = await browser.newPage();
+
+	await page.goto("https://example.com");
+	await page.screenshot({ path: "example.png" });
+
+	await browser.close();
+})();
