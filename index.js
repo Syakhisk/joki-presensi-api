fa32bdb3949390633b7cfe01273238caa8f5de98
syakhisk <mail.syakhisk@gmail.com>
2021-03-02 19:02:03 +0700

    send example.com body

--- index.js
+++ index.js
@@ -6,25 +6,40 @@ const getBody = async () => {
 		defaultViewport: null,
 		args: ["--incognito", "--no-sandbox", "--single-process", "--no-zygote"],
 	};
-	const browser = await puppeteer.launch(chromeOptions);
-	const page = await browser.newPage();
+	// const browser = await puppeteer.launch(chromeOptions);
+	// const page = await browser.newPage();
 
-	await page.goto("https://example.com");
-	let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-	// await page.screenshot({ path: "example.png" });
-	console.log(bodyHTML);
-	await browser.close();
+	// await page.goto("https://example.com");
+	// let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+	// // await page.screenshot({ path: "example.png" });
+	// console.log(bodyHTML);
+	// await browser.close();
+	try {
+		const browser = await puppeteer.launch(chromeOptions);
+		const page = await browser.newPage();
 
-	return bodyHTML;
+		await page.goto("https://example.org/", { waitUntil: "networkidle0" });
+		const data = await page.evaluate(() => document.body.innerHTML);
+
+		// console.log(data);
+
+		await browser.close();
+		return data;
+	} catch (err) {
+		console.error(err);
+	}
+
+	// return bodyHTML;
 };
 
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", function (req, res) {
-	res.send(getBody);
+app.get("/", async function (req, res) {
+	const theData = await getBody();
+	// console.log(theData);
+	res.send(theData);
 });
 
-app.listen(process.env.PORT || port, () =>
-	console.log(`Listening at port ${port}`)
-);
+app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
