970f8e62a9a94c09536c35792fe6ce7219487982
syakhisk <mail.syakhisk@gmail.com>
2021-03-03 02:41:23 +0700

    adding new routes

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/* ------------------------------ routes import ----------------------------- */
+const indexRouter = require("./routes/index");
+const presensiRoute = require("./routes/presensi");
+const updateRoute = require("./routes/update");
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/* ------------------------------ routes ----------------------------- */
+app.use("/", indexRouter);
+app.use("/v1/presensi", presensiRoute);
+app.use("/v1/update", updateRoute);
 
 module.exports = app;
