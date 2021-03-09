7673294c8b79b1d3657b46972d0b53c3b0c9e4c0
syakhisk <mail.syakhisk@gmail.com>
2021-03-09 13:25:53 +0700

    added presensi functionality

--- routes/presensi.js
+++ routes/presensi.js
@@ -1,13 +1,41 @@
-const login = require('../lib/pptr/Login');
 const express = require("express");
+const Hadir = require("../lib/pptr/Hadir");
 const router = express.Router();
 
-/* GET users listing. */
+const login = require("../lib/pptr/Login");
+const isEmpty = require("../lib/ValidateObject");
+
+const storeUsage = require("../service/StoreUsage");
+
 router.post("/", async function (req, res, next) {
-	// res.send(req.body);
+	// res.status(202).send({ msg: "work" });
+	// return;
+
+	const { nrp, password, selectedClass, code, date } = req.body;
+	if (!nrp || !password || !selectedClass || !code || !date) {
+		res.status(400).send({ msg: "Bad request, missing body." });
+		return;
+	}
+
+	/** Check if the request body is empty */
+	if (isEmpty(req.body)) {
+		res.status(400).send({ msg: "Bad request, empty body." });
+		return;
+	}
+
+	/** Check if login success*/
 	const { page, browser } = await login(req.body);
-  // absen ke presensi
-  // send response
+	if (!page || !browser) {
+		res.status(404).send({
+			msg:
+				"User not found, check whether you are using incorrect username or password",
+		});
+		return;
+	}
+
+	const presensi = await Hadir(req.body, page, browser);
+	console.log("presensi response:");
+	res.status(presensi.code).send({ msg: presensi.msg });
 });
 
 module.exports = router;
